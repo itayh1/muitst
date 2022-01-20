2a2cc98ea4b19c4f4477b40d9f97cdaa184b3540
Itay Hassid <itayh.msn@hotmail.com.com>
2022-01-21 00:01:43 +0200

    Add anonymous auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, ButtonGroup, Checkbox, FormControlLabel } from "@mui/material";
+
+import { ThemeProvider, createTheme } from "@mui/material";
+import { green, orange } from "@mui/material/colors";
+
+import { Typography, Container, Paper, Grid, AppBar, Toolbar, IconButton } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+
+import "fontsource-roboto";
+
+import SortableList from "./component/SortableList";
+import Navbar from "./component/Navbar";
+import Enter from "./component/EnterPage";
+import OwnsPage from "./component/OwnsPage";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
+import "firebase/auth";
+import "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+// if (!firebase.apps.length) {
+initializeApp({
+    apiKey: "AIzaSyA5reNeZ4nPxn0agS6na6rLKBVjoYN_uGE",
+    authDomain: "website-64644.firebaseapp.com",
+    projectId: "website-64644",
+    storageBucket: "website-64644.appspot.com",
+    messagingSenderId: "848566509101",
+    appId: "1:848566509101:web:a983d2fc84e5220a63a2a4",
+});
+
+const db = getFirestore();
+
+const theme = createTheme({
+    typography: { h5: { fontSize: 25, margin: "10px" } },
+    palette: {
+        primary: {
+            main: orange[500],
+        },
+        secondary: {
+            main: green[600],
+        },
+    },
+});
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        const uid = user.uid;
+        // setIsConnected(true);
+        console.log("logged in with user: " + uid);
+    } else {
+        // setIsConnected(false);
+        console.log("logged out");
+    }
+});
+
+function getOrSetGroup() {}
+
+function CheckboxExample() {
+    const [checked, setChecked] = useState(true);
+    return <FormControlLabel control={<Checkbox checked={checked} onChange={(e) => setChecked(e.target.checked)} />} label={"Test checkbox"} />;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isConnected, setIsConnected] = useState(false);
+    const [user] = useAuthState(auth);
+    const signIn = () =>
+        signInAnonymously(auth)
+            .then(() => {
+                setIsConnected(true);
+                console.log("Signed In");
+            })
+            .catch((error) => {
+                console.log(error.code);
+                console.log(error.message);
+            });
+    const deals = {
+        groupName: "Nugat",
+        owns: [
+            { name: "Itay", items: ["Meat", "Water"], prices: [50, 30] },
+            { name: "Leon", items: ["Fuel"], prices: [100] },
+        ],
+    };
+    return (
+        <ThemeProvider theme={theme}>
+            {/* <Container maxWidth="sm"> */}
+            <Navbar />
+            <div className="App">
+                <header className="App-header">
+                    {isConnected && user ? <OwnsPage {...deals} /> : <Enter signIn={signIn} />}
+                    {/* <AppBar color="secondary">
+                            <Toolbar>
+                                <IconButton>
+                                    <Menu />
+                                </IconButton>
+                                <Typography style={{ flexGrow: 1 }} variant="h6">
+                                    MUI Theme
+                                </Typography>
+                                <Button variant="contained">Login</Button>
+                            </Toolbar>
+                        </AppBar> */}
+                    {/* <Typography variant="h5">Welcome to test site of MaterialUI</Typography>
+
+                    <Grid container spacing={4} justifyContent={"center"}>
+                        <Grid item xs={3}>
+                            <Paper style={{ height: 75, width: "100%" }} />
+                        </Grid>
+                        <Grid item xs={3} sm={6}>
+                            <Paper style={{ height: 75, width: "100%" }} />
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Paper style={{ height: 75, width: "100%" }} />
+                        </Grid>
+                    </Grid>
+                    <CheckboxExample />
+                    <ButtonGroup>
+                        <Button size="large" variant="contained" color="primary">
+                            Upload
+                        </Button>
+                        <Button size="large" variant="contained" color="secondary">
+                            Download
+                        </Button>
+                    </ButtonGroup>
+                    <SortableList /> */}
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+            </div>
+            {/* </Container> */}
+        </ThemeProvider>
+    );
 }
 
 export default App;
